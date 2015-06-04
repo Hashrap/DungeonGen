02f19dfcdbd4507adbf411b401bbce4b48682ad1
Spencer Corkran <corkransp@gmail.com>
2015-06-03 21:11:46 -0400

    hotfix

--- Game1/Properties/AssemblyInfo.cs
+++ Game1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Game1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DungeonGen")]
+[assembly: AssemblyDescription("Open-Source dungeon generation")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Game1")]
+[assembly: AssemblyProduct("DungeonGen")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
